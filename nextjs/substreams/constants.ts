d47f9ae7b378f67deedc7fd761f792576560e0b7
SohZHong <sohzhehong09@gmail.com>
2025-03-12 10:31:33 +0800

    added substreams config files

---
+++ nextjs/substreams/constants.ts
@@ -0,0 +1,7 @@
+export const TOKEN: string = process.env.SUBSTREAMS_API_TOKEN || '';
+export const ENDPOINT = 'https://mainnet.eth.streamingfast.io';
+export const SPKG =
+  'https://spkg.io/streamingfast/ethereum-explorer-v0.1.2.spkg';
+export const MODULE = 'tx_counter_view';
+export const START_BLOCK = 325320000;
+export const STOP_BLOCK = '+100';
